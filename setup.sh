2856d576b9bb48c4c6fa17c882e49fa5d0826417
Andy_Tsai <Andy_Tsai@quantatw.com>
2022-01-11 10:08:35 +0800

    add .profile

--- setup.sh
+++ setup.sh
@@ -1,8 +1,10 @@
 cd ~
 rm .vimrc
 rm .bashrc
+rm .profile
 ln -s ~/dotfiles/.vimrc .vimrc
 ln -s ~/dotfiles/.bashrc .bashrc
+ln -s ~/dotfiles/.profile .profile
 
 #Install Vim
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
