ab0354b7ddf9fe527e1e0f4023aa0cd9832b1d9d
Andy_Tsai <Andy_Tsai@quantatw.com>
2022-07-22 16:07:09 +0800

    2022/07/22 Make setup.sh more easily to add dotfiles

--- setup.sh
+++ setup.sh
@@ -1,12 +1,16 @@
+DOTFILES=(
+    .vimrc
+    .bashrc
+    .profile
+    .gitconfig
+)
+
 cd ~
-rm .vimrc 2>/dev/null
-rm .bashrc 2>/dev/null
-rm .profile 2>/dev/null
-rm .gitconfig 2>/dev/null
-ln -s ~/dotfiles/.vimrc .vimrc
-ln -s ~/dotfiles/.bashrc .bashrc
-ln -s ~/dotfiles/.profile .profile
-ln -s ~/dotfiles/.gitconfig .gitconfig
+rm ${DOTFILES[*]} 2>/dev/null
+for i in ${DOTFILES[@]}
+do
+    ln -s ~/dotfiles/$i $i
+done
 
 #Install Vim
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
